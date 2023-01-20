b4ab707708b6798636dc8b33ad9709aa5d07f500
sahishnusuresh <sahishnusuresh@gmail.com>
2023-01-20 15:21:34 +0530

    interfaces

---
+++ interfaces/src/index.ts
@@ -0,0 +1,7 @@
+const app = document.getElementById("app");
+if (app) {
+  app.innerHTML = `
+  <h1>Interfaces</h1>
+  <p>The code for this practice is in <code>./src/interfaces.ts</code>. Complete the instructions in that file, and make sure your types are all passing.
+  `;
+}
