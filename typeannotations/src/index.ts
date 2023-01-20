dce50cdb6d536904eb37fb5268f75b24b0f4dc72
sahishnusuresh <sahishnusuresh@gmail.com>
2023-01-20 15:01:23 +0530

    adding type annotations

---
+++ typeannotations/src/index.ts
@@ -0,0 +1,7 @@
+const app = document.getElementById("app");
+if (app) {
+  app.innerHTML = `
+  <h1>Type Annotations</h1>
+  <p>The code for this practice is in the <code>src/annotations.ts</code> file. All you have to do is open that file up and add the necessary type annotations to remove the red squiggles.
+  `;
+}
